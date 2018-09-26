0a0a0fa7fa0a3f4736df12121ee761321d555d3a
wangui.njoka <wangui.njoka@gmail.com>
2018-09-26 15:05:17 +0300

    created my models

--- photos/models.py
+++ photos/models.py
@@ -1,8 +1,34 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    title = models.CharField(max_length = 30)
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+class Photo(models.Model):
+    name = models.CharField(max_length = 30)
     description = models.TextField()
     location = models.ForeignKey(Location)
     category =models.ForeignKey(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    photo_image = models.ImageField(upload_to = 'photos/')
--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
     url('^$',views.photos,name = 'photos'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
