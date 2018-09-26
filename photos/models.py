0e66b3881aa584dc86405ce692e6ea55ec968368
wangui.njoka <wangui.njoka@gmail.com>
2018-09-26 13:07:40 +0300

    added postgres database

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length = 30)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category =models.ForeignKey(Category)
